e2da04a2f5a14fb755dea79f68c72f208392c1c2
Kyleds63 <kstoner63@gmail.com>
2017-05-10 09:31:08 -0500

    Started Calendar for Profile

--- app/public/js/submitPlan.js
+++ app/public/js/submitPlan.js
@@ -1,6 +1,3 @@
-// Code here handles what happens when a user submits a new character on the form.
-// Effectively it takes the form inputs then sends it to the server to save in the DB.
-
 // when user clicks add-btn
 $("#planSubmit").on("click", function(event) {
   event.preventDefault();
